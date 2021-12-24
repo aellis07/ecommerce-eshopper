9a7eef898696b29c4f3d8db1ec6fa0ae097a1d8d
Anthony Ellis <amellis245@gmail.com>
2021-12-23 23:35:55 -0500

    changed key-value pair key name

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,34 +2,34 @@ const { Product } = require("../models");
 
 const productData = [
   {
-    productName: "Plain T-Shirt",
+    product_name: "Plain T-Shirt",
     price: 14.99,
     stock: 14,
-    categoryID: 1,
+    category_id: 1,
   },
   {
-    productName: "Running Sneakers",
+    product_name: "Running Sneakers",
     price: 90.0,
     stock: 25,
-    categoryID: 5,
+    category_id: 5,
   },
   {
-    productName: "Branded Baseball Hat",
+    product_name: "Branded Baseball Hat",
     price: 22.99,
     stock: 12,
-    categoryID: 4,
+    category_id: 4,
   },
   {
-    productName: "Top 40 Music Compilation Vinyl Record",
+    product_name: "Top 40 Music Compilation Vinyl Record",
     price: 12.99,
     stock: 50,
-    categoryID: 3,
+    category_id: 3,
   },
   {
-    productName: "Cargo Shorts",
+    product_name: "Cargo Shorts",
     price: 29.99,
     stock: 22,
-    categoryID: 2,
+    category_id: 2,
   },
 ];
 
